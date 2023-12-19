233d4c5330ef1d18a4741ffadd616fe867bbbff8
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2023-12-19 13:03:33 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,23 @@
 content = tab('index.html') // https://petercastell.ca/editor
+
+console.log("path: " + path);
+
+if(path == "") {
+	content = tab('index.html')
+}
+else if(path == "post") {
+  	if(query.password == "123") {
+    	content = "Posted succesfully"
+    	save("save_state", body)
+    }
+  	else {
+    	content = "Wrong Password Loser"
+    }
+}
+else if(path == "get") {
+  content = load("save_state", "{}")
+  console.log(content)
+}
+else {
+  content = "404"
+}
