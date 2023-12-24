fd12a51285d9ed384859e276a3cd5fd3526557f4
Henry Lin <77286137+Nugget-exe@users.noreply.github.com>
2023-12-24 11:40:00 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,8 +2,13 @@ content = tab('index.html') // https://petercastell.ca/editor
 
 console.log("path: " + path);
 
+const proj_name = document.getElementById("project-name").value
+
 if(path == "") {
-	content = tab('index.html')
+	content = tab('index.html').replace("PROJNAME", proj_name)
+}
+else if(path == "test.html") {
+  content = tab('test.html').replace("PROJNAME", proj_name)
 }
 else if(path == "post") {
   	if(query.password == "123") {
