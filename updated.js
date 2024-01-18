56df9426e6d69fd49e0f42660bdc8111cb2a9dae
Henry Lin <77286137+Nugget-exe@users.noreply.github.com>
2024-01-18 12:25:00 -0500

    Create updated.js

---
+++ updated.js
@@ -0,0 +1,26 @@
+content = tab('index.html') // https://petercastell.ca/editor
+
+console.log("path: " + path);
+
+const proj_name = document.getElementById("project-name").value
+
+if(path == ""|| path == "index.html") {
+	content = tab('index.html')//.replace("PROJNAME", proj_name)
+}
+
+else if(path == "post") {
+  	if(query.password == "123") {
+    	content = "Posted succesfully"
+    	save("save_state", body)
+    }
+  	else {
+    	content = "Wrong Password Loser"
+    }
+}
+else if(path == "get") {
+  content = load("save_state", "{}")
+  console.log(content)
+}
+else {
+  content = "404"
+}
